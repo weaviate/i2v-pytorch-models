11154e1e4821bcaef7b12b69dd261d91e21469ab
Stefan Bogdan <stefanbogdan180394@gmail.com>
2022-01-21 11:56:58 +0100

    Add uvicorn logger instead of prints, add startup even, fixes #2

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
+import os
+from logging import getLogger
 from fastapi import FastAPI, Response, status
 from vectorizer import ImageVectorizer, VectorImagePayload
-import os
+
 
 app = FastAPI()
+imgVec : ImageVectorizer
+logger = getLogger('uvicorn')
 
-cuda_env = os.getenv("ENABLE_CUDA")
-cuda_support=False
-cuda_core=""
-if cuda_env is not None and cuda_env == "true" or cuda_env == "1":
-    cuda_support=True
-    cuda_core = os.getenv("CUDA_CORE")
-    if cuda_core is None or cuda_core == "":
-        cuda_core = "cuda:0"
-    print("[INFO] cuda core set to {}".format(cuda_core))
-else:
-    print("[INFO] running on CPU")
 
-imgVec = ImageVectorizer(cuda_support, cuda_core)
+@app.on_event("startup")
+def startup_event():
+	global imgVec
+
+	cuda_env = os.getenv("ENABLE_CUDA")
+	cuda_support = False
+	cuda_core = ""
+
+	if cuda_env is not None and cuda_env == "true" or cuda_env == "1":
+		cuda_support = True
+		cuda_core = os.getenv("CUDA_CORE")
+		if cuda_core is None or cuda_core == "":
+				cuda_core = "cuda:0"
+		logger.info(f"CUDA_CORE set to {cuda_core}")
+	else:
+		logger.info("Running on CPU")
+
+	imgVec = ImageVectorizer(cuda_support, cuda_core)
+
 
 @app.get("/.well-known/live", response_class=Response)
 @app.get("/.well-known/ready", response_class=Response)
 def live_and_ready(response: Response):
-  response.status_code = status.HTTP_204_NO_CONTENT
+	response.status_code = status.HTTP_204_NO_CONTENT
+
 
 @app.post("/vectors")
 def read_item(item: VectorImagePayload, response: Response):
-  try:
-    vector = imgVec.vectorize(item.id, item.image)
-    return {"id": item.id, "vector": vector.tolist(), "dim": len(vector)}
-  except Exception as e:
-    response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    return {"error": str(e)}
+	try:
+		vector = imgVec.vectorize(item.id, item.image)
+		return {"id": item.id, "vector": vector.tolist(), "dim": len(vector)}
+	except Exception as e:
+		logger.exception(
+            'Something went wrong while vectorizing data.'
+        )
+		response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+		return {"error": str(e)}
